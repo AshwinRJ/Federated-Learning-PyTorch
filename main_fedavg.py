a52b69c93e57c22dd4d9eff5e4135090220cb044
AshwinRJ <ashwinr001@gmail.com>
2018-11-25 23:21:26 -0500

    Added avg acc plot

--- main_fedavg.py
+++ main_fedavg.py
@@ -163,6 +163,14 @@ if __name__ == '__main__':
     plt.savefig('../save/fed_{}_{}_{}_C{}_iid{}_loss.png'.format(args.dataset,
                                                                  args.model, args.epochs, args.frac, args.iid))
 
+    # Plot Average Accuracy vs Communication rounds
+    plt.figure()
+    plt.title('Average Accuracy vs Communication rounds')
+    plt.plot(range(len(train_accuracy)), train_accuracy, color='k')
+    plt.ylabel('Average Accuracy')
+    plt.xlabel('Communication Rounds')
+    plt.savefig('../save/fed_{}_{}_{}_C{}_iid{}_acc.png'.format(args.dataset,
+                                                                args.model, args.epochs, args.frac, args.iid))
     # testing (original)
     list_acc, list_loss = [], []
     net_glob.eval()
