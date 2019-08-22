4874c36d30bea481315c8dafebb56b766b2162e3
AshwinRJ <ashwinr001@gmail.com>
2019-08-22 16:26:38 -0400

    Added support for Fashion Mnist

--- Federated_Avg/FedNets.py
+++ Federated_Avg/FedNets.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Python version: 3.6
 
-import torch
 from torch import nn
 import torch.nn.functional as F
 
@@ -42,6 +41,29 @@ class CNNMnist(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+class CNNFashion_Mnist(nn.Module):
+    def __init__(self, args):
+        super(CNNFashion_Mnist, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(7*7*32, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
 class CNNCifar(nn.Module):
--- Federated_Avg/options.py
+++ Federated_Avg/options.py
@@ -7,39 +7,47 @@ import argparse
 
 def args_parser():
     parser = argparse.ArgumentParser()
-    # federated arguments
-    parser.add_argument('--epochs', type=int, default=10, help="rounds of training")
-    parser.add_argument('--num_users', type=int, default=100, help="number of users: K")
-    parser.add_argument('--frac', type=float, default=0.1, help='the fraction of clients: C')
-    parser.add_argument('--local_ep', type=int, default=5, help="the number of local epochs: E")
-    parser.add_argument('--local_bs', type=int, default=10, help="local batch size: B")
-    parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
-    parser.add_argument('--momentum', type=float, default=0.5, help='SGD momentum (default: 0.5)')
+
+    # federated arguments (Notation for the arguments followed from paper)
+    parser.add_argument('--epochs', type=int, default=10,
+                        help="number of rounds of training")
+    parser.add_argument('--num_users', type=int, default=100,
+                        help="number of users: K")
+    parser.add_argument('--frac', type=float, default=0.1,
+                        help='the fraction of clients: C')
+    parser.add_argument('--local_ep', type=int, default=5,
+                        help="the number of local epochs: E")
+    parser.add_argument('--local_bs', type=int, default=10,
+                        help="local batch size: B")
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.5,
+                        help='SGD momentum (default: 0.5)')
 
     # model arguments
     parser.add_argument('--model', type=str, default='mlp', help='model name')
-    parser.add_argument('--kernel_num', type=int, default=9, help='number of each kind of kernel')
+    parser.add_argument('--kernel_num', type=int, default=9,
+                        help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
                         help='comma-separated kernel size to use for convolution')
+    parser.add_argument('--num_channels', type=int, default=1, help="number of channels of imgs")
     parser.add_argument('--norm', type=str, default='batch_norm',
                         help="batch_norm, layer_norm, or None")
     parser.add_argument('--num_filters', type=int, default=32,
-                        help="number of filters for conv nets -- 32 for miniimagenet, 64 for omiglot.")
+                        help="number of filters for conv nets -- 32 for mini-imagenet, 64 for omiglot.")
     parser.add_argument('--max_pool', type=str, default='True',
                         help="Whether use max pooling rather than strided convolutions")
 
     # other arguments
     parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
-    parser.add_argument('--iid', type=int, default=0,
-                        help='whether i.i.d or not, 1 for iid, 0 for non-iid')
-    parser.add_argument('--unequal', type=int, default=0,
-                        help='in non-i.i.d, whether data split among clients is equal or not, 1 for unequal split')
     parser.add_argument('--num_classes', type=int, default=10, help="number of classes")
-    parser.add_argument('--num_channels', type=int, default=1, help="number of channels of imgs")
     parser.add_argument('--gpu', type=int, default=1, help="GPU ID")
+    parser.add_argument('--iid', type=int, default=0,
+                        help='whether i.i.d or not: 1 for iid, 0 for non-iid')
+    parser.add_argument('--unequal', type=int, default=0,
+                        help='whether to use unequal data splits for  non-i.i.d setting (use 0 for equal splits)')
     parser.add_argument('--stopping_rounds', type=int, default=10, help='rounds of early stopping')
-    parser.add_argument('--verbose', type=int, default=1,
-                        help='verbose print, 1 for True, 0 for False')
+    parser.add_argument('--verbose', type=int, default=1, help='verbose')
     parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
     args = parser.parse_args()
     return args
