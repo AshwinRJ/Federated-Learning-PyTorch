2d3b7d23371d35859942387478b69aa04c093b76
Ashwin R Jadhav <ashwinrj@cmu.edu>
2019-09-01 14:18:45 -0400

    Minor update

--- src/options.py
+++ src/options.py
@@ -29,27 +29,36 @@ def args_parser():
     parser.add_argument('--kernel_num', type=int, default=9,
                         help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
-                        help='comma-separated kernel size to use for convolution')
-    parser.add_argument('--num_channels', type=int, default=1, help="number of channels of imgs")
+                        help='comma-separated kernel size to \
+                        use for convolution')
+    parser.add_argument('--num_channels', type=int, default=1, help="number \
+                        of channels of imgs")
     parser.add_argument('--norm', type=str, default='batch_norm',
                         help="batch_norm, layer_norm, or None")
     parser.add_argument('--num_filters', type=int, default=32,
-                        help="number of filters for conv nets -- 32 for mini-imagenet, 64 for omiglot.")
+                        help="number of filters for conv nets -- 32 for \
+                        mini-imagenet, 64 for omiglot.")
     parser.add_argument('--max_pool', type=str, default='True',
-                        help="Whether use max pooling rather than strided convolutions")
+                        help="Whether use max pooling rather than \
+                        strided convolutions")
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
-    parser.add_argument('--num_classes', type=int, default=10, help="number of classes")
+    parser.add_argument('--dataset', type=str, default='mnist', help="name \
+                        of dataset")
+    parser.add_argument('--num_classes', type=int, default=10, help="number \
+                        of classes")
     parser.add_argument('--gpu', default=None, help="To use cuda, set \
                         to a specific GPU ID. Default set to use CPU.")
-    parser.add_argument('--optimizer', type=str, default='sgd', help="type of optimizer")
-    parser.add_argument('--iid', type=int, default=0,
-                        help='whether i.i.d or not: 1 for iid, 0 for non-iid')
+    parser.add_argument('--optimizer', type=str, default='sgd', help="type \
+                        of optimizer")
+    parser.add_argument('--iid', type=int, default=1,
+                        help='Default set to IID. Set to 0 for non-IID.')
     parser.add_argument('--unequal', type=int, default=0,
-                        help='whether to use unequal data splits for  non-i.i.d setting (use 0 for equal splits)')
-    parser.add_argument('--stopping_rounds', type=int, default=10, help='rounds of early stopping')
+                        help='whether to use unequal data splits for  \
+                        non-i.i.d setting (use 0 for equal splits)')
+    parser.add_argument('--stopping_rounds', type=int, default=10,
+                        help='rounds of early stopping')
     parser.add_argument('--verbose', type=int, default=1, help='verbose')
-    parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+    parser.add_argument('--seed', type=int, default=1, help='random seed')
     args = parser.parse_args()
     return args
