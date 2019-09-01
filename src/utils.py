778f45d0b72f978cfd0726a72bef20f248266eac
Ashwin R Jadhav <ashwinrj@cmu.edu>
2019-09-01 14:20:33 -0400

    Added exp_details function

--- src/utils.py
+++ src/utils.py
@@ -82,3 +82,21 @@ def average_weights(w):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
+
+
+def exp_details(args):
+    print('\nExperimental details:')
+    print(f'    Model     : {args.model}')
+    print(f'    Optimizer : {args.optimizer}')
+    print(f'    Learning  : {args.lr}')
+    print(f'    Global Rounds   : {args.epochs}\n')
+
+    print('    Federated parameters:')
+    if args.iid:
+        print('    IID')
+    else:
+        print('    Non-IID')
+    print(f'    Fraction of users  : {args.frac}')
+    print(f'    Local Batch size   : {args.local_bs}')
+    print(f'    Local Epochs       : {args.local_ep}\n')
+    return
