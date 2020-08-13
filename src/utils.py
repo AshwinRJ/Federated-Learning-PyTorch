74a097d69dce4e1eec20c471e81a53d30a807a51
Jamshid Tursunboyev <tursunboevjamshid@gmail.com>
2020-08-14 00:20:49 +0900

    Fix dataset selection for cifar
    Change dataset selection for the cifar option from MNIST to CIFAR10

--- src/utils.py
+++ src/utils.py
@@ -21,10 +21,10 @@ def get_dataset(args):
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
+        train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
                                        transform=apply_transform)
 
-        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
+        test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
                                       transform=apply_transform)
 
         # sample training data amongst users
